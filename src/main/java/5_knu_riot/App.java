94422ecfc1dae1d9fec3b55e4810ba3a8e765a0c
Hongjin Kim <jin3430@gmail.com>
2018-10-02 21:22:35 +0900

    first commit

---
+++ src/main/java/5_knu_riot/App.java
@@ -0,0 +1,13 @@
+package 5_knu_riot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
