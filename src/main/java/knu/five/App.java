5496fea1497c326d09e545877045ad9844c66912
Hongjin Kim <jin3430@gmail.com>
2018-10-04 22:29:16 +0900

    change App.java

--- src/main/java/knu/five/App.java
+++ src/main/java/knu/five/App.java
@@ -1,13 +1,28 @@
 package knu.five;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello! I'm Kim Hong Jin!" );
+import java.util.Scanner;
+
+public class App {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("5조");
+        hoyoung();
+        juhwan();
+        mongjin();
+        shinhyuk();
+    }
+
+    // 이름 나이를 포함해서 소개한다.
+    public static void hoyoung(){
+
+    }
+    public static void juhwan(){
+
+    }
+    public static void mongjin() {
+
+    }
+    public static void shinhyuk() {
+
     }
 }
